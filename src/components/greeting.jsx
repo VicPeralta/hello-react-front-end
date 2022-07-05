7f834080bc9122d296ee9cd9db05772ddc07e03b
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-07-04 20:01:36 -0500

    Add greeting component
    modified:   src/App.js
    new file:   src/components/greeting.jsx
    modified:   src/index.js

---
+++ src/components/greeting.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { getGreeting } from '../redux/greetings/greetings'
+// import './HelloWorld.css'
+
+function Greeting() {
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(getGreeting());
+  // }, []);
+  // const greeting = useSelector((state) => state.greeting);
+  // const fetching = useSelector((state) => state.fetching);
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default Greeting;
