4cac3a6929cae97157eccc3a7e4afd169b1c1c8e
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-07-04 21:10:37 -0500

    Add redux, store, reducer, router
    modified:   package-lock.json
    modified:   package.json
    modified:   src/App.js
    new file:   src/components/greeting.css
    modified:   src/components/greeting.jsx
    modified:   src/index.js
    new file:   src/redux/configureStore.js
    new file:   src/redux/greeting/reducer.js

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useEffect } from 'react';
-// import { getGreeting } from '../redux/greetings/greetings'
-// import './HelloWorld.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getGreeting } from '../redux/greeting/reducer';
+import './greeting.css';
 
 function Greeting() {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(getGreeting());
-  // }, []);
-  // const greeting = useSelector((state) => state.greeting);
-  // const fetching = useSelector((state) => state.fetching);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, []);
+  const greeting = useSelector((state) => state.greeting);
+  const fetching = useSelector((state) => state.fetching);
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="greeting">
+      {!fetching && (<h1>{greeting}</h1>)}
+      {fetching && (<h2>Fetching your personalized greeting</h2>)}
     </div>
   );
 }
